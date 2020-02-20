4b02c682b1b7e53bd48cc11b2187f90ff427cf8a
unknown <empociask@dons.usfca.edu>
2020-02-19 23:37:29 -0800

    Finished functionality

---
+++ DBoperations.py
@@ -0,0 +1,55 @@
+import psycopg2
+from config import *
+from reddit import *
+import time
+import datetime
+
+class DBoperations:
+
+    def __init__(self) -> None:
+        self.conn = None
+        self.cur = None
+
+    def connect(self) -> None:
+        try:
+
+            params = config()
+            print("Connecting to postgreSQL database")
+            self.conn = psycopg2.connect(**params)
+
+            self.cur = self.conn.cursor()
+
+        except(Exception, psycopg2.DatabaseError) as error:
+            print("Error : ", error)
+
+    def getTrendingRedditData(self):
+        self.cur.execute("SELECT NAME FROM STATIC_MARKET_DATA;")
+        x = self.cur.fetchall()
+
+        returnList = []
+        for i in x:
+            returnList.append(str(i)[2: str(i).find(',') - 1])
+
+        st = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
+
+        for coinName in returnList:
+            dataFrame = getSubredditData(coinName)
+            if dataFrame is not None:
+                for index, row in dataFrame.iterrows():
+                    print(row['id'])
+                    try:
+                        print(row)
+                        self.cur.execute(
+                            "INSERT INTO REDDIT_DATA(ID, NAME, TIME_STAMP, TITLE, SCORE, SUBREDDIT, URL, NUM_COMMENTS, BODY, CREATED) VALUES('{}','{}','{}','{}','{}','{}','{}','{}','{}','{}')".format(
+                                row['id'], coinName, st, row['title'].join(e for e in row['title'] if e.isalnum()).replace("\'", ""), row['score'], row['subreddit'], row['url'],
+                                row['num_comments'], row['body'].join(e for e in row['body'] if e.isalnum()).replace("\'", ""), row['created']))
+
+                    except Exception as e:
+                        print("ERROR ", e)
+
+                self.conn.commit()
+
+
+temp = DBoperations()
+temp.connect()
+temp.getTrendingRedditData()
---
+++ reddit.py
@@ -0,0 +1,21 @@
+from hidden import *
+import praw
+import pandas as pd
+
+
+def getSubredditData(subName):
+    reddit = praw.Reddit(client_id=clientId, client_secret=pk, user_agent=userAgent)
+    posts = []
+    try:
+        subreddit = reddit.subreddit(subName)
+
+    except Exception as e:
+        print("ERROR: ", e)
+        return None
+
+    for post in subreddit.hot(limit=100):
+        posts.append(
+            [post.title, post.score, post.id, post.subreddit, post.url, post.num_comments, post.selftext, post.created])
+    posts = pd.DataFrame(posts, columns=['title', 'score', 'id', 'subreddit', 'url', 'num_comments', 'body', 'created'])
+
+    return posts
